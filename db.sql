9570df0da7ce34dd952826165bdbf7843a610124
FAT553 <115791661+FAT553@users.noreply.github.com>
2025-07-17 09:44:04 +0300

    Create db.sql
    Create database

---
+++ db.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE vulnapp;
+
+USE vulnapp;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50),
+    password VARCHAR(50)
+);
+
+INSERT INTO users (username, password) VALUES
+('admin', 'admin123'), 
+('user', 'user123');
